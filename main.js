30200b2ccf6e10900c0dca32c603426d9544899f
Manish Rane <manishrane182@gmail.com>
2022-10-09 00:02:32 +0530

    Array Methods

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const foods = ['pizza', 'burger', 'fingerChips', 'donuts', 'springRoll']
 //Function #1:
 function arraySlice(foods){
     modified=foods
+    console.log(foods)
     console.log(modified.slice(1,4))
 }
 
 //Function #2:
 function arraySplice(foods){
     modified=foods
+    console.log(foods)
     modified.splice(2,0,'noodles','icecream')
     console.log(modified)
 }
@@ -17,6 +19,7 @@ const numberArray = [12,324,213,4,2,3,45,4234];
 
 //Function #3:
 function filter(numberArray){
+    console.log(numberArray)
     console.log(numberArray.filter((val)=>{
         return (val%2)==0
     }))
@@ -24,8 +27,9 @@ function filter(numberArray){
 
 //Function #4:
 function isPrime(numberArray){
+    console.log(numberArray)
     console.log(numberArray.filter((val)=>{
-        if (val>2){
+        if (val>=2){
         let state=false
         for (var i=2;i<=Math.abs(Math.sqrt(val));i++){
             if (val%i==0){
@@ -36,13 +40,16 @@ function isPrime(numberArray){
         if (state==false){
             return val
         }
-    }}))
+    }
+}))
 }
+
 
 const myArray = [11, 34, 20, 5, 53, 16]
 
 //Function #5:
 function map(Arr){
+    console.log(myArray)
     console.log(Arr.map((val)=> val*val))
 }
 
@@ -50,6 +57,10 @@ const Array=[2,3,5,10]
 
 //Function #6:
 function Reduce(Arr){
+    console.log(Array)
     console.log(Arr.reduce((init,item)=> init*item))
 }
-Reduce(Array)
+
+function clean(){
+    console.clear()
+}
